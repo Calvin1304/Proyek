753291eb035e27d9868f8927fec8be8eea288138
Calvin1304 <jnatan1304@gmail.com>
2024-05-29 11:12:00 +0700

    Add files via upload

---
+++ Calvin/login.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="style.css">
+   </head>
+   <link rel="stylesheet" href="login.css">
+</head>
+   <body>
+      <div class="wrapper">
+         <div class="title">
+            Login Form
+         </div>
+         <form action="#">
+            <div class="field">
+               <input type="text" required>
+               <label>Username</label>
+            </div>
+            <div class="field">
+               <input type="password" required>
+               <label>Password</label>
+            </div>
+            <div class="field">
+               <input type="text" required>
+               <label>Email Address</label>
+            </div>
+            <div class="field">
+               <input type="text" required>
+               <label>Role</label>
+            </div>
+            <div class="field">
+               <input type="text" required>
+               <label>Gender</label>
+            </div>
+            <div class="field">
+               <input type="text" required>
+               <label>Hobi</label>
+            </div>
+            <div class="content">
+               <div class="checkbox">
+                  <!-- <input type="checkbox" id="remember-me"> -->
+                  <!-- <label for="remember-me">Remember me</label> -->
+               </div>
+               <div class="pass-link">
+                  <!-- <a href="#">Forgot password?</a> -->
+               </div>
+            </div>
+            <div class="field">
+               <input type="submit" value="Login">
+            </div>
+            <div class="signup-link">
+               <!-- Not a member? <a href="#">Signup now</a> -->
+            </div>
+         </form>
+      </div>
+   </body>
+</html>
+<!-- <!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="login.css">
+</head>
+<style>
+h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+</head>
+<body>
+<div>
+    <body><div class="container">
+        <form action="Register.html">
+          <div class="row">
+            <h2 style="text-align:center">LOGIN</h2>
+            <div class="vl">
+            </div>
+            <div class="col">
+              <div class="hide-md-lg">
+              <input type="text" name="Username" placeholder="username" required="">
+            </div>
+              <input type="text" name="Email" placeholder="Email" required="">
+            </div>
+              <input type="password" name="password" placeholder="Password" required="">
+              </div>
+<body>
+<button class="button button1">Login</button>
+<div>
+<button>Register</button>
+</head>
+<body>
+    
+</body>
+</html> -->
